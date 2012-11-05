175bff177da6ea5e39e6f7e25b74100471edec13
jeonghwan <jeonghwan@jeonghwan-ui-MacBook.local>
2012-11-05 20:04:25 +0900

    Merge 2 buttons into 1 button

--- change-image.js
+++ change-image.js
@@ -1,34 +1,46 @@
+// 전역 변수
 var imgCount = -1;
 const imgNum = 3
 var timer;
+var isChange = false;
     
-function changeImg() {
+// 이미지를 img element에 로딩
+function loadImg() {
     imgCount = (imgCount + 1) % imgNum;
     document.getElementsByTagName("img")[0].src = "sea" + imgCount + ".jpeg";            
 }     
 
+// 이미지 크기를 창 크기에 맞춤
 function trimImageSize() {
     var imageHeight = document.body.clientHeight - 50;
     document.getElementsByTagName("img")[0].style.height = imageHeight;
 }
 
-function start() {
-    trimImageSize();
-    timer = setInterval(function(){changeImg()}, 1000);            
+// 버튼 핸들러
+function btnHandler() {
+    if (isChange) {
+        // stop
+        clearInterval(timer);
+        document.getElementById("btn").value = "Change Images!";
+    } else {
+        // start
+        timer = setInterval(function(){loadImg()}, 1000);            
+        document.getElementById("btn").value = "stop";
+    }
+    
+    isChange = !isChange;
 }
 
-function stop() {
-    clearInterval(timer);
-}
-
+// (1) body 로딩시 콜백함수 연결 및 버튼 설정 
+// (2) 초기 이미지 로딩 및 크기 조절
 window.onload = function() {
-    document.getElementById("changebtn").onclick = function() {start()};
-    document.getElementById("stopbtn").onclick = function() {stop()};            
-                
-    changeImg();
+    document.getElementById("btn").value = "Change images!";
+    document.getElementById("btn").onclick = function() {btnHandler()};
+    loadImg();
     trimImageSize();
 }   
 
+// 윈도우 사이즈 변경시 콜백함수 연결
 window.onresize = function() {
     trimImageSize();
 }
